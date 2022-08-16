46c4e73f30a21e4b2aa075c9b71331b60fd89c33
victoroli432 <110979951+victoroli432@users.noreply.github.com>
2022-08-15 23:54:45 -0300

    Update webappauto.py

--- webappauto.py
+++ webappauto.py
@@ -7,10 +7,12 @@ st.sidebar.title('Menu')
 paginaselecionada = st.sidebar.selectbox('selecione a pagina que deseja',['pagina 1','pagina 2'])
 
 if paginaselecionada == 'pagina 1':
-    st.title('Autocust')
-    st.selectbox('selecione uma opção',['opção 1','opção 2'])
+    opçãoselecionada = st.selectbox('selecione uma opção',['opção 1','opção 2'])
 elif paginaselecionada == 'pagina 2':
-    st.title('Autocust')
     st.write('Souto você é o bambambam')
 
+if opçãoselecionada == 'opção 1':
+    st.write('vinicião meu pau na sua mão')
+elif opçãoselecionada == 'opção 2':
+    st.select_slider('escolhe um ai',['victor','vinicius','soutinho da galera'])
 
