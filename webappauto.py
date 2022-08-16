0d3aac80382f0f42eee0405b6d8a39f337039e77
victoroli432 <110979951+victoroli432@users.noreply.github.com>
2022-08-16 07:53:03 -0300

    Update webappauto.py

--- webappauto.py
+++ webappauto.py
@@ -15,7 +15,7 @@ hashed_passwords = stauth.hasher(passwords).generate()
 authenticator = stauth.authenticate(names,usernames,hashed_passwords,'cookie_name', 'signature_key',cookie_expiry_days=30)
 name, authentication_status = authenticator.login('Login','sidebar')
 if authentication_status:
- st.write(‘Welcome *%s*’ % (name))
+ st.write(‘Welcome (name)')
  # your application
 elif authentication_status == False:
  st.error(‘Username/password is incorrect’)
