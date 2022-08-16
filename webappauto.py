6ca6baffda39c9b9417499bd74e062c7847ae5a5
victoroli432 <110979951+victoroli432@users.noreply.github.com>
2022-08-15 23:43:57 -0300

    Update webappauto.py

--- webappauto.py
+++ webappauto.py
@@ -10,9 +10,7 @@ paginaselecionada=st.sidebar._selectbox('selecione a pagina que deseja',['pagina
 if paginaselecionada == 'pagina 1':
     st.title('Autocust')
 
-st.selectbox('selecione uma opção',['opção 1','opção 2'])
-
 elif paginaselecionada == 'pagina 2':
     st.title('Autocust')
-
 st.write('Souto você é o bambambam')
+
