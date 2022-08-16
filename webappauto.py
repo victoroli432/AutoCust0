879748df78f64dea8f903c979d9462bb2c0431f5
victoroli432 <110979951+victoroli432@users.noreply.github.com>
2022-08-16 07:51:08 -0300

    Update webappauto.py

--- webappauto.py
+++ webappauto.py
@@ -1,9 +1,28 @@
+from imaplib import _Authenticator
 import streamlit as st
+import streamlit _Authenticator as stauth
 
 st.title('Autocust')
 
 
 st.sidebar.title('Menu')
+
+names = ['victor oliveira','vinicius oliveira']
+usernames = ['victoroli','viniciusoli']
+passwords = ['123a','4567']
+
+hashed_passwords = stauth.hasher(passwords).generate()
+authenticator = stauth.authenticate(names,usernames,hashed_passwords,'cookie_name', 'signature_key',cookie_expiry_days=30)
+name, authentication_status = authenticator.login('Login','sidebar')
+if authentication_status:
+ st.write(‘Welcome *%s*’ % (name))
+ # your application
+elif authentication_status == False:
+ st.error(‘Username/password is incorrect’)
+elif authentication_status == None:
+ st.warning(‘Please enter your username and password’)
+ 
+
 paginaselecionada = st.sidebar.selectbox('selecione a pagina que deseja',['pagina 1','pagina 2'])
 
 if paginaselecionada == 'pagina 1':
@@ -15,4 +34,5 @@ if paginaselecionada == 'pagina 1':
 
 elif paginaselecionada == 'pagina 2':
     st.write('Souto você é o bambambam')
+
 
