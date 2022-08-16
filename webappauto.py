47cb3bfaa0cfa40187dbf4bb666cd24955735930
victoroli432 <110979951+victoroli432@users.noreply.github.com>
2022-08-15 23:31:57 -0300

    Update webappauto.py

--- webappauto.py
+++ webappauto.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
-st.title('AutoCust')
+st.title('Autocust')
+
+st.selectbox('selecione uma opção',['opção 1','opção 2'])
+
+st.sidebar.title('Menu')
+paginaselecionada=st.sidebar._selectbox('selecione a pagina que deseja',['pagina 1','pagina 2'])
+
+if paginaselecionada == 'pagina 1':
+    st.title('Autocust')
+
+st.selectbox('selecione uma opção',['opção 1','opção 2'])
+
+elif paginaselecionada == 'pagina 2':
+    st.title('Autocust')
+st.write('souto você é o bambambam')
