5a022ede84b2a0007c1bee43c30896995a5874b5
victoroli432 <110979951+victoroli432@users.noreply.github.com>
2022-10-21 10:54:38 -0300

    Update webappauto.py

--- webappauto.py
+++ webappauto.py
@@ -31,10 +31,11 @@ elif psd == 'Tabela de gastos':
 
             clientecontroller.incluir(cliente)
 elif psd == 'Calculadora de Rendimento':
-    st.title('Calculadora de Rendimento:')          
+    st.title('Calculadora de Rendimento:')   
+    with st.form(key='Rendimento'):
              input_litros = st.number_input(label='insira a quantidade de litros',step=0.5)
              input_km = st.number_input(label='insira a kilometragem percorrida',step=0.5)
-            botao = st.button('Calcular')
+             botao st.form_submit_button('Calcular')
             calculo = (input_litros/input_km)
         if botão:
             st.write(f'Litros: {input_litros}')
